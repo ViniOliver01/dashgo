5c5333f2f55ec07799176ffbb6df5e86dd5ff150
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-11-28 20:01:01 -0300

    Updated useUsers hook

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query"
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query"
 import { api } from "../api"
 
 interface User {
@@ -44,6 +44,6 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 
 export function useUsers(page: number){
    return useQuery(['users', page], () => getUsers(page), {
-      staleTime: 1000 * 60 * 10 // 10 Minutos
+      staleTime: 1000 * 60 * 10, // 10 Minutos
    })
 }
