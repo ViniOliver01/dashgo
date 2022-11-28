83cb9ba0f71415292364067e146519de680d7910
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-11-28 17:02:07 -0300

    Updated users page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
 import { makeServer } from './../services/mirage/index';
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 if (process.env.NODE_ENV == 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient()
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SidebarDrawerProvider>
-      <ChakraProvider resetCSS theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SidebarDrawerProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarDrawerProvider>
+        <ChakraProvider resetCSS theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SidebarDrawerProvider>
+    </QueryClientProvider>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import Header from '../../components/Header';
@@ -6,8 +6,17 @@ import Sidebar from '../../components/Sidebar';
 import Pagination from '../../components/Pagination';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import { useQuery } from 'react-query'
 
 export default function UserList() {
+
+   const { data, isLoading, error } = useQuery('users', async () => {
+      const response = await fetch('http://localhost:3000/api/users')
+      const data = await response.json()
+
+      return data
+   })
+
    const isWideVersion = useBreakpointValue({
       base: false,
       lg: true,
@@ -50,33 +59,45 @@ export default function UserList() {
                      </Button>
                   </Link>
                </Flex>
-               <Table colorScheme="whiteAlpha">
-                  <Thead>
-                     <Tr>
-                        <Th paddingInline={["4", "4", "6"]} color="gray.300" width="8">
-                           <Checkbox colorScheme="pink" />
-                        </Th>
-                        <Th>Usuário</Th>
-                        {isWideVersion && <Th>Data de cadastro</Th>}
-                     </Tr>
-                  </Thead>
-                  <Tbody>
-                     <Tr>
-                        <Td paddingInline={["4", "4", "6"]}>
-                           <Checkbox colorScheme="pink" />
-                        </Td>
-                        <Td>
-                           <Box>
-                              <Text fontWeight="bold">Vinicius Oliveira</Text>
-                              <Text fontSize="small" color="gray.300">vinioliver.dev@gmail.com</Text>
-                           </Box>
-                        </Td>
-                        {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                     </Tr>
+               { isLoading ? (
+                  <Flex justify="center">
+                     <Spinner />
+                  </Flex>
+               ) : error ? (
+                  <Flex justify="center">
+                     <Text>Falha ao obter dados dos usuários</Text>
+                  </Flex>
+               ) : (
+                  <>
+                     <Table colorScheme="whiteAlpha">
+                        <Thead>
+                           <Tr>
+                              <Th paddingInline={["4", "4", "6"]} color="gray.300" width="8">
+                                 <Checkbox colorScheme="pink" />
+                              </Th>
+                              <Th>Usuário</Th>
+                              {isWideVersion && <Th>Data de cadastro</Th>}
+                           </Tr>
+                        </Thead>
+                        <Tbody>
+                           <Tr>
+                              <Td paddingInline={["4", "4", "6"]}>
+                                 <Checkbox colorScheme="pink" />
+                              </Td>
+                              <Td>
+                                 <Box>
+                                    <Text fontWeight="bold">Vinicius Oliveira</Text>
+                                    <Text fontSize="small" color="gray.300">vinioliver.dev@gmail.com</Text>
+                                 </Box>
+                              </Td>
+                              {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                           </Tr>
 
-                  </Tbody>
-               </Table>
-               <Pagination />
+                        </Tbody>
+                     </Table>
+                     <Pagination />
+                  </>
+               )}
             </Box>
          </Flex>
       </Box>
